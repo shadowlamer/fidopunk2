847f3d9ea4853a97dfa7357a40cf9057ec82b2d7
Vadim Cherenev <sl@anhot.ru>
2025-02-25 00:21:19 +0500

    parse command

--- main.c
+++ main.c
@@ -3,21 +3,27 @@
 
 #define CMD_LENGTH 128
 
-static char cmd_buf[128];
+static char cmd_buf[CMD_LENGTH];
+
+static char *argv[10];
+int argc;
 
 static const char cursor[] = {0x12,0x1,'_',0x12,0x00,0x08};
 static const char prompt[] = {'\n','$',' '};
 
 
 int read_cmd();
+void parse_cmd();
 void print(char *s, int len);
+void printz(char *s);
 
 int main() {
   int len;
   init_stdio();
   for (;;) {
     len = read_cmd();
-    print(cmd_buf, len);
+    parse_cmd();
+    printz(argv[0]);
   }
 }
 
@@ -32,6 +38,7 @@ int read_cmd() {
     switch (key) {
       case 0x0d:  
         putchar (' ');
+        cmd_buf[len] = 0x00;
         return len;
       case 0x08:
         if (len <= 0) break;
@@ -52,4 +59,19 @@ int read_cmd() {
 
 void print(char *s, int len) {
   for (int i=0; i<len; i++) putchar(s[i]);
+}
+
+void printz(char *s) {
+    for (char *p = s; *p != 0x00; p++) putchar(*p);
+}
+
+void parse_cmd() {
+  argc = 0;
+  for (char *p = cmd_buf; *p != 0x00; p++) {
+    if (*p == ' ') {
+      *p == 0x00;
+      argv[argc] = p;
+      argc++;
+    }
+  }
 }
