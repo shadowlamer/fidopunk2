4f2d4e9fd9a55d468733e10a832d25bb09109cd2
sl <v.cherenev@seuslab.ru>
2025-03-27 16:39:47 +0500

    intro screen

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ PROJECT="fidopunk"
 TAPE="${PWD}/${PROJECT}.tap"
 ROM="${PWD}/bin/main.c.rom"
 CODE="${PWD}/main.bin"
-#INTRO="${PWD}/assets/intro.scr"
+INTRO="${PWD}/echo.scr"
 SKIP=536
 
 END=$(grep -oba $'\xde\xad\xbe\xef' ${ROM} | cut -d: -f1)
@@ -15,4 +15,4 @@ zmakebas -i 1 -a 10 -l -o ${TAPE} -n ${PROJECT} loader.bas
 # Install dependencies
 [ -d node_modules ] || npm install jsbin2tap
 
-node_modules/.bin/jsbin2tap -p -o ${TAPE} ${CODE}
+node_modules/.bin/jsbin2tap -p -o ${TAPE} ${INTRO} ${CODE}
--- loader.bas
+++ loader.bas
@@ -1,2 +1,8 @@
+border 0
+paper 0
+ink 7
+clear 24290
+poke 23739,111
+load "" screen$
 load "" code 24291
 randomize usr 24291
