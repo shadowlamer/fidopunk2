287c11d1fa262b4413e2462f4e50d80fa2952de8
Vadim Cherenev <sl@anhot.ru>
2025-03-31 17:41:30 +0500

    snake2

--- snake.c
+++ snake.c
@@ -5,20 +5,53 @@ __at (ATTR_SCREEN_BUFFER_START) char attributes[ATTR_SCREEN_BUFFER_SIZE];
 
 #define SNAKE_MAX_SIZE 10
 #define SNAKE_MIN_SIZE 3
+#define MAX_ENCRYPTED_CHARS 10
 
 typedef enum {
   DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT
 } t_direction;
 
+typedef struct {
+  unsigned char x;
+  unsigned char y;
+  char symbol;
+} t_encrypted_char;
+
 t_point snake_body[SNAKE_MAX_SIZE];
 int snake_size = SNAKE_MIN_SIZE;
 t_direction snake_direction = DIR_UP;
+t_encrypted_char encrypted_chars[MAX_ENCRYPTED_CHARS];
+int current_char = 0;
+static unsigned int seed = 12345;
+
+unsigned int random() {
+  __asm
+  ld hl, (_seed)
+  ld a,r
+  ld d,a
+  ld e,(hl)
+  add hl,de
+  add a,l
+  xor h
+  ld (_seed),hl
+  __endasm;
+}
+
+void highlight_char(int index) {
+    attributes[encrypted_chars[index].y * SCR_CHAR_WIDTH + encrypted_chars[index].x] = 0b00000010;
+}
 
 void init_game() {
   for (int i=0; i<SNAKE_MIN_SIZE; i++) {
     snake_body[i].x = SCR_CHAR_WIDTH / 2;
     snake_body[i].y = SCR_CHAR_HEIGHT / 2;
   }
+  for (int i = 0; i < MAX_ENCRYPTED_CHARS; i++) {
+       encrypted_chars[i].x = random() % SCR_CHAR_WIDTH;
+       encrypted_chars[i].y = random() % SCR_CHAR_HEIGHT;
+       putchar_at('#', encrypted_chars[i].x, encrypted_chars[i].y, DEFAULT_ATTR);
+  }
+  highlight_char(current_char);
 }
 
 void snake_draw_body() {
@@ -28,7 +61,7 @@ void snake_draw_body() {
 }
 
 void snake_move() {
-    attributes[snake_body[snake_size - 1].y * SCR_CHAR_WIDTH + snake_body[snake_size - 1].x] = 0b00000100;
+    attributes[snake_body[snake_size - 1].y * SCR_CHAR_WIDTH + snake_body[snake_size - 1].x] = DEFAULT_ATTR;
     for (int i = snake_size - 1; i > 0; i--) {
         snake_body[i].y = snake_body[i - 1].y;
         snake_body[i].x = snake_body[i - 1].x;
