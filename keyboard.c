43d50b38b36b08562f2ef4a670347948e6c0c550
Vadim Cherenev <sl@anhot.ru>
2025-04-02 14:55:47 +0500

    yyy

--- keyboard.c
+++ keyboard.c
@@ -1,46 +1,54 @@
 #include "keyboard.h"
 
+// Раскладка клавиатуры
 static t_keyboard_row keyboard_layout[KEYBOARD_NUM_ROWS] = {
-  { // 0
-    .port = 0xfe, 
-    .keys = {0xff, 'z', 'x', 'c', 'v'},
-    .caps = {0xff, 'Z', 'X', 'C', 'V'},
-    .symb = {0xff, ':', '`', '?', '/'}},
-  { // 1
-    .port = 0xfd, 
-    .keys = {'a', 's', 'd', 'f', 'g'},
-    .caps = {'A', 'S', 'D', 'F', 'G'},
-    .symb = {'~', '|', '\\', '{', '}'}},
-  { // 2
-    .port = 0xfb, 
-    .keys = {'q', 'w', 'e', 'r', 't'},
-    .caps = {'Q', 'W', 'E', 'R', 'T'},
-    .symb = {0x0, 0x0, 0x0, '<', '>'}},
-  { // 3
-    .port = 0xf7, 
-    .keys = {'1', '2', '3', '4', '5'},
-    .caps = {0x0, 0x0, 0x0, 0x0, K_LT},
-    .symb = {'!', '@', '#', '$', '%'}},
-  { // 4
-    .port = 0xef, 
-    .keys = {'0', '9', '8', '7', '6'},
-    .caps = {K_DL, 0x0, K_RT, K_UP, K_DN},
-    .symb = {'_', ')', '(', '\'', '&'}},
-  { // 5
-    .port = 0xdf, 
-    .keys = {'p', 'o', 'i', 'u', 'y'},
-    .caps = {'P', 'O', 'I', 'U', 'Y'},
-    .symb = {'\"', ';', 0x0, ']', '['}},
-  { // 6
-    .port = 0xbf, 
-    .keys = {'\n', 'l', 'k', 'j', 'h'},
-    .caps = {'\n', 'L', 'K', 'J', 'H'},
-    .symb = {'\n', '=', '+', '-', '^'}},
-  { // 7y
-    .port = 0x7f, 
-    .keys = {' ', 0xff, 'm', 'n', 'b'},
-    .caps = {' ', 0xff, 'M', 'N', 'B'},
-    .symb = {' ', 0xff, '.', '\,', '*'}},
+    [0] = { // Row 0
+        .port = 0xFE, 
+        .keys = {K_MOD, 'z', 'x', 'c', 'v'},
+        .caps = {K_MOD, 'Z', 'X', 'C', 'V'},
+        .symb = {K_MOD, ':', '`', '?', '/'}},
+    
+    [1] = { // Row 1
+        .port = 0xFD, 
+        .keys = {'a', 's', 'd', 'f', 'g'},
+        .caps = {'A', 'S', 'D', 'F', 'G'},
+        .symb = {'~', '|', '\\', '{', '}'}},
+    
+    [2] = { // Row 2
+        .port = 0xFB, 
+        .keys = {'q', 'w', 'e', 'r', 't'},
+        .caps = {'Q', 'W', 'E', 'R', 'T'},
+        .symb = {'\0', '\0', '\0', '<', '>'}},
+    
+    [3] = { // Row 3
+        .port = 0xF7, 
+        .keys = {'1', '2', '3', '4', '5'},
+        .caps = {'\0', '\0', '\0', '\0', K_LT},
+        .symb = {'!', '@', '#', '$', '%'}},
+    
+    [4] = { // Row 4
+        .port = 0xEF, 
+        .keys = {'0', '9', '8', '7', '6'},
+        .caps = {K_DL, '\0', K_RT, K_UP, K_DN},
+        .symb = {'_', ')', '(', '\'', '&'}},
+    
+    [5] = { // Row 5
+        .port = 0xDF, 
+        .keys = {'p', 'o', 'i', 'u', 'y'},
+        .caps = {'P', 'O', 'I', 'U', 'Y'},
+        .symb = {'"', ';', '\0', ']', '['}},
+    
+    [6] = { // Row 6
+        .port = 0xBF, 
+        .keys = {'\n', 'l', 'k', 'j', 'h'},
+        .caps = {'\n', 'L', 'K', 'J', 'H'},
+        .symb = {'\n', '=', '+', '-', '^'}},
+    
+    [7] = { // Row 7
+        .port = 0x7F, 
+        .keys = {' ', K_MOD, 'm', 'n', 'b'},
+        .caps = {' ', K_MOD, 'M', 'N', 'B'},
+        .symb = {' ', K_MOD, '.', ',', '*'}}
 };
 
 char scan(unsigned char port) {
@@ -61,8 +69,8 @@ char getkey() {
     char key = 0; // Переменная для хранения кода нажатой клавиши (результат)
 
         // Переменные для отслеживания состояния модификаторов клавиш:
-    unsigned char caps_shift = (scan(0xfe) & 0x01) == 0;  // Флаг Caps Shift (например, для переключения регистра)
-    unsigned char symbol_shift = (scan(0x7f) & 0x02) == 0; // Флаг Symbol Shift (например, для специальных символов)
+    unsigned char caps_shift = (scan(0xfe) & 0x01) == 0;  // Флаг Caps Shift
+    unsigned char symbol_shift = (scan(0x7f) & 0x02) == 0; // Флаг Symbol Shift
 
     // Цикл по всем строкам клавиатуры
     for (unsigned char r = 0; r < KEYBOARD_NUM_ROWS; r++) {
--- keyboard.h
+++ keyboard.h
@@ -5,12 +5,14 @@
 #define KEYBOARD_NUM_ROWS 8
 #define KEYBOARD_KEYS_PER_ROW 5
 
-#define K_TB 0x06
-#define K_DL 0x07
-#define K_LT 0x08
-#define K_RT 0x09
-#define K_UP 0x0B
-#define K_DN 0x0C
+// Специальные символы
+#define K_TB 0x06 // Tab
+#define K_DL 0x07 // Delete
+#define K_LT 0x08 // Left Arrow
+#define K_RT 0x09 // Right Arrow
+#define K_UP 0x0B // Up Arrow
+#define K_DN 0x0C // Down Arrow
+#define K_MOD 0xFF // Скан-код модификатора
 
 typedef struct {
   unsigned char port;
